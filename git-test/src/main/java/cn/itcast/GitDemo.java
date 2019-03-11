65780fe26a67a8e3afddbe72be7da84e2402481c
wtn <wtn@163.com>
2019-03-11 20:51:19 +0800

    创建gittest类

---
+++ git-test/src/main/java/cn/itcast/GitDemo.java
@@ -0,0 +1,11 @@
+package cn.itcast;
+
+public class GitDemo {
+    public static void main(String[] args) {
+
+        System.out.println("11111");
+        System.out.println("22222");
+        System.out.println("33333");
+
+    }
+}
